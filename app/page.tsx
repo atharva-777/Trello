308c77f5d4beb001412104a26247d7007e70deda
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-06-06 12:04:51 +0530

    removed src directory

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-3xl font-bold underline'>Hello Nextjs</h1>
-      <h3 className='text-5md font-serif bg-slate-400 text-pink-800'>Hey Here mini ME tailwind css</h3>
+      <h1>Next js - Appwrite - Tailwind CSS - Zustand</h1>
     </main>
   )
 }
