a229dab0d7b53ea590fea4c600304f470e43f876
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-06-07 14:42:00 +0530

    appwrite cloud database connection,type defination for board cols, database structure, state management via BoardStore

--- Components/Board.tsx
+++ Components/Board.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+"use client"; 
+import {useBoardStore} from '@/store/BoardStore';
+import React, { useEffect } from 'react'
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 const Board = () => {
+
+  const getBoard = useBoardStore((state)=>state.getBoard);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard])
+  
   return (
-    <div>Board</div>
+    <h1 className='text-gray-200 bg-gray-950'>Hello Board</h1>
+      // <DragDropContext>
+      //   <Droppable droppableId='board' direction='horizontal' type='column'>
+      //     {(provided)=>(
+      //       <div>
+      //         {/* rendering all the columns */}
+      //       </div>
+      //     )}
+      //   </Droppable>
+
+      // </DragDropContext>
   )
 }
 
