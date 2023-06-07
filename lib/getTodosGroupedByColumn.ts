7a51b8a90300e53bf22469c4ad4fc8850b5dfe0d
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-06-07 15:23:01 +0530

    sorted todos according to status

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -1,9 +1,27 @@
-import { databases } from "@/appwrite"
+import { databases } from "@/appwrite";
 
 export const getTodosGroupedByColumn = async () => {
-    const data = await databases.listDocuments(
-      process.env.NEXT_PUBLIC_TRELLO_DATABASE_ID!,
-      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
-    );
-    console.log(data)
-} 
+  const data = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_TRELLO_DATABASE_ID!,
+    process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
+  );
+  const todos = data.documents;
+  console.log(todos)
+  const columns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+    return acc;
+  }, new Map<TypedColumn, Column>());
+  console.log(columns)
+};
